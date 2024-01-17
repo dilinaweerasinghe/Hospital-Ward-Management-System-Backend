f90400ee0d466061cbfdb988daaea691025c0c99
dilina683 <weerasinghe.dilina683@gmail.com>
2024-01-17 13:14:04 +0530

    Folder structure

---
+++ src/main/java/group17/HospitalWardManagementSystem/HospitalWardManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package group17.HospitalWardManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalWardManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalWardManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group17/HospitalWardManagementSystem/HospitalWardManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package group17.HospitalWardManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalWardManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
