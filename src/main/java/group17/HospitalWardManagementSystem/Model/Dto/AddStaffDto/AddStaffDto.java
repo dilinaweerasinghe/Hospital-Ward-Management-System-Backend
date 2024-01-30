9ebfddcfd9425422fe28ac3d1b2bdf2ba7b9d7da
Edirisinhage <nipunaheshansadu@gmail.com>
2024-01-30 15:00:19 +0530

    Create AddStaffDto

---
+++ src/main/java/group17/HospitalWardManagementSystem/Model/Dto/AddStaffDto/AddStaffDto.java
@@ -0,0 +1,36 @@
+package group17.HospitalWardManagementSystem.Model.Dto.AddStaffDto;
+
+import group17.HospitalWardManagementSystem.Model.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddStaffDto {
+
+    private String nic;
+    private String fullName;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private LocalDate dob;
+    private String email;
+    private UserRole Position;
+
+
+    private String mobileNo;
+
+}
