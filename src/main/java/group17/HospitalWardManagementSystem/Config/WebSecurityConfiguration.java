8a30cffc15c34dbffa9ae11037598e42ec165d9c
dilina683 <weerasinghe.dilina683@gmail.com>
2024-01-30 21:43:17 +0530

    Update Spring security with jwt token

--- src/main/java/group17/HospitalWardManagementSystem/Config/WebSecurityConfiguration.java
+++ src/main/java/group17/HospitalWardManagementSystem/Config/WebSecurityConfiguration.java
@@ -1,13 +1,15 @@
 package group17.HospitalWardManagementSystem.Config;
 
+import group17.HospitalWardManagementSystem.Service.Login.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,49 +21,45 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration{
     @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    @Autowired
-    private JwtRequestFilter jwtRequestFilter;
-
-    @Autowired
-    private UserDetailsService jwtService;
+    private JwtRequestFilter jwtFilter;
 
     @Bean
-    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception{
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults());
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/authenticate").permitAll()
-                                .requestMatchers("/registerNewUser").permitAll()
-                                .requestMatchers("/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    public UserDetailsService userDetailsService(){
+        return new UserInfoService();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/authenticate")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
-        authenticationManagerBuilder.userDetailsService(jwtService).passwordEncoder(passwordEncoder());
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/group17/HospitalWardManagementSystem/Controller/LoginController.java
+++ src/main/java/group17/HospitalWardManagementSystem/Controller/LoginController.java
@@ -4,17 +4,18 @@ package group17.HospitalWardManagementSystem.Controller;
 import group17.HospitalWardManagementSystem.Model.Domain.User;
 import group17.HospitalWardManagementSystem.Model.Dto.JwtRequest;
 import group17.HospitalWardManagementSystem.Model.Dto.JwtResponse;
-import group17.HospitalWardManagementSystem.Model.Dto.LoginDto;
 import group17.HospitalWardManagementSystem.Service.Login.JwtService;
-import group17.HospitalWardManagementSystem.Service.Login.LoginService;
 import group17.HospitalWardManagementSystem.Service.Login.RegistrationService;
+import group17.HospitalWardManagementSystem.Service.Login.UserInfoService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LoginController {
@@ -22,13 +23,26 @@ public class LoginController {
 
     @Autowired
     private JwtService jwtService;
-
+    @Autowired
+    private AuthenticationManager authenticationManager;
     @Autowired
     private RegistrationService registrationService;
+    @Autowired
+    private UserInfoService userInfoService;
+
 
     @PostMapping("/authenticate")
-    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception{
-        return jwtService.CreateJwtToken(jwtRequest);
+    public String createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception{
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
+        if(authenticate.isAuthenticated()){
+            System.out.println("Here");
+            String token =  jwtService.generateToken(jwtRequest.getUsername());
+            Optional<User> user = userInfoService.getUser(jwtRequest.getUsername());
+            return token;
+
+        }else {
+            throw new UsernameNotFoundException("Invalid user request");
+        }
     }
 
     @PostMapping("/registerNewUser")
