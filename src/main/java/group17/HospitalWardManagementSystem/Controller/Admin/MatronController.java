37102a1f34197f929d8ff94150ee99d2c3b3ed7d
dilina683 <weerasinghe.dilina683@gmail.com>
2024-02-24 10:47:58 +0530

    Add addMatronDto

--- src/main/java/group17/HospitalWardManagementSystem/Controller/Admin/MatronController.java
+++ src/main/java/group17/HospitalWardManagementSystem/Controller/Admin/MatronController.java
@@ -1,4 +1,11 @@
 package group17.HospitalWardManagementSystem.Controller.Admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Admin")
 public class MatronController {
+
 }
