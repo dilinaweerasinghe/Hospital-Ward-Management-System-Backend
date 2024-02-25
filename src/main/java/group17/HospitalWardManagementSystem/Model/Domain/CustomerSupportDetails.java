4c164707e3e9c11078cd082bcf3f16d4d31cfed6
Edirisinhage <nipunaheshansadu@gmail.com>
2024-02-25 21:30:53 +0530

    created customer support Controller

--- src/main/java/group17/HospitalWardManagementSystem/Model/Domain/CustomerSupportDetails.java
+++ src/main/java/group17/HospitalWardManagementSystem/Model/Domain/CustomerSupportDetails.java
@@ -14,6 +14,7 @@ public class CustomerSupportDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long customer_complain_id;
+    @Column(nullable = false)
     private String nic;
     private String username;
     private String email;
