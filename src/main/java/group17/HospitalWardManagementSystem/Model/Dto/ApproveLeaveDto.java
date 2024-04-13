4b15517c3859a42d3787ddf9ee0bf61c2f689bb0
dilina683 <weerasinghe.dilina683@gmail.com>
2024-04-13 17:05:30 +0530

    Add Approved leaves repository

--- src/main/java/group17/HospitalWardManagementSystem/Model/Dto/ApproveLeaveDto.java
+++ src/main/java/group17/HospitalWardManagementSystem/Model/Dto/ApproveLeaveDto.java
@@ -1,4 +1,30 @@
 package group17.HospitalWardManagementSystem.Model.Dto;
 
+import group17.HospitalWardManagementSystem.Model.Domain.Staff;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
 public class ApproveLeaveDto {
+    private int leaveId;
+
+    private String leaveNo;
+    private String name;
+
+    private LocalDate leaveBeginDate;
+
+    private LocalDate leaveEndDate;
+
+    private LocalDate requestedDate;
+
+
 }
